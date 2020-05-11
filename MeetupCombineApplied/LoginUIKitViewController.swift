08b6b2b1e3a738773a050f620b4e0d9f956845db
David Céspedes <ingdavidcespedes@gmail.com>
2020-05-11 14:15:42 -0500

    First Commit

---
+++ MeetupCombineApplied/LoginUIKitViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginUIKitViewController.swift
+//  MeetupCombineApplied
+//
+//  Created by David Andres Cespedes on 5/11/20.
+//  Copyright © 2020 David Andres Cespedes. All rights reserved.
+//
+
+import UIKit
+
+class LoginUIKitViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MeetupCombineAppliedTests/MeetupCombineAppliedTests.swift
@@ -0,0 +1,34 @@
+//
+//  MeetupCombineAppliedTests.swift
+//  MeetupCombineAppliedTests
+//
+//  Created by David Andres Cespedes on 5/11/20.
+//  Copyright © 2020 David Andres Cespedes. All rights reserved.
+//
+
+import XCTest
+@testable import MeetupCombineApplied
+
+class MeetupCombineAppliedTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
